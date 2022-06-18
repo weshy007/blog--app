1f389ac6b2e278868931044f3a67568c55bcb723
weshy <josephwaweru96@gmail.com>
2022-06-19 02:03:13 +0300

    pagination, class views/mixin

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,43 @@
-from django.shortcuts import render, get_object_or_404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
+
 from .models import Post
 
-# Create your views here.
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'list.html', {'posts': posts})
 
-def post_detail(request, year, month, day):
+# Create your views here.
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'list.html'
+
+    
+def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # 3 posts per page
+    page = request.GET.get('page')
+    try:
+        post = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+
+    context = {
+        'page': page,
+        'posts': posts,
+    }
+    return render(request, 'list.html', context)
+
+
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
                                     status='published',
                                     publish__year=year,
                                     publish__month=month,
                                     publish__day=day)
 
-    return render(request, 'detail.html', {'post': post})
+    return render(request, 'list_detail.html', {'post': post})
