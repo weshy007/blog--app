236dfd8ed7e754be7bfd517bc9b9db992a25cc84
weshy <josephwaweru96@gmail.com>
2022-06-21 17:42:56 +0300

    - search query - stemming, ranking, weighting queries - searching with trigram similarity

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (SearchQuery, SearchRank,
+                                            SearchVector, TrigramSimilarity)
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Count
@@ -127,8 +128,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') +\
+                            SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector('title', 'body')).filter(search=query)
+                similarity=TrigramSimilarity('title', query),
+                ).filter(similarity__gt=0.1).order_by('-similarity')
 
     context = {
         'form': form,
